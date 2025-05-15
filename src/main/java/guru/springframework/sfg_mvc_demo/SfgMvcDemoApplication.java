9017e5b7b3d987a396d8424452de44a349d698cf
sergei <sergei.gots@yahoo.com>
2025-05-15 20:01:20 +0300

    Initial commit

---
+++ src/main/java/guru/springframework/sfg_mvc_demo/SfgMvcDemoApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfg_mvc_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgMvcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgMvcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfg_mvc_demo/SfgMvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfg_mvc_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMvcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
