afdf996f011a7b01275ed2924bdbd3d0564c6fb8
sergei <sergei.gots@yahoo.com>
2025-05-16 15:27:35 +0300

    404: Map.of("message", "error") as @ResponseBody returned

--- src/main/java/guru/springframework/sfg_mvc_demo/exception/GlobalExceptionHandler.java
+++ src/main/java/guru/springframework/sfg_mvc_demo/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 /**
@@ -17,7 +18,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
-    public String handleNoSuchElementException(NoSuchElementException e) {
-        return "Resource not found: " + e.getMessage();
+    public Map<String, String> handleNoSuchElementException(NoSuchElementException e) {
+        return Map.of(
+                "error", "Resource not found",
+                "message", e.getMessage()
+        );
     }
 }
