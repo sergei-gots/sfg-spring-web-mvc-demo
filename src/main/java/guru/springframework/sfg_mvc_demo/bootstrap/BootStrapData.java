e372f12b2f9c6cbe086ff60e3284e64983baf76c
sergei <sergei.gots@yahoo.com>
2025-05-16 14:13:36 +0300

    BootStrapData added

---
+++ src/main/java/guru/springframework/sfg_mvc_demo/bootstrap/BootStrapData.java
@@ -0,0 +1,44 @@
+package guru.springframework.sfg_mvc_demo.bootstrap;
+
+import guru.springframework.sfg_mvc_demo.domain.Customer;
+import guru.springframework.sfg_mvc_demo.repositories.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by sergei on 16/05/2025
+ */
+@Component
+@Slf4j
+public class BootStrapData implements CommandLineRunner {
+
+    private final CustomerRepository customerRepository;
+
+    public BootStrapData(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        log.info("Loading customers");
+
+        customerRepository.save(createCustomer("Michael", "Westen"));
+        customerRepository.save(createCustomer("Fiona", "Glennane"));
+        customerRepository.save(createCustomer("Sam", "Axe"));
+
+        log.info("{} customers have been successfully created and saved to the database", customerRepository.count());
+
+    }
+
+    private Customer createCustomer(String firstName, String lastName) {
+
+        Customer customer = new Customer();
+        customer.setFirstname(firstName);
+        customer.setLastname(lastName);
+
+        return customer;
+    }
+
+}
